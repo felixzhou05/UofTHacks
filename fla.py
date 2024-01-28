f9de760ec469d34c2dc0b1bbf8d8af125d3f78fc
Bobby Yan <77584562+t0te1234@users.noreply.github.com>
2024-01-28 07:16:17 -0500

    Add files via upload

--- fla.py
+++ fla.py
@@ -1,10 +1,18 @@
+from flask_cors import CORS
 from datetime import datetime
+from datetime import date
 import requests
-from flask import Flask, redirect, request, jsonify, session
-from flask import Flask, redirect, session
+from flask import *
+from flask import Flask, request, render_template, redirect, url_for
+from fileinput import filename 
+import os
+from werkzeug.utils import secure_filename
+from main import findSong
+import random
 
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = 'dasdio12909kpok'
 
 client_id = '64f1524ada334d40a89ddf08b6a67a00'
@@ -17,32 +25,50 @@ SPOTIFY_REDIRECT_URI = 'http://localhost:5000/callback'
 SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
 SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
 SPOTIFY_API_BASE_URL = 'https://api.spotify.com/v1/'
+app.config['UPLOAD_FOLDER'] = 'static/images/'
+global is_member
+is_member = True
+
 
 auth_url = "https://accounts.spotify.com/api/authorize"
 token_url = "https://accounts.spotify.com/api/token"
 api_base_url = "https://api.spotify.com/v1/"
 
+
 @app.route('/')
 def index():
-    return "Welcome <a href='/login'>Login</a>"
+    return "Welcome <a href='/login'>Login</a> & <a href='/memberin'>Login-member</a> & <a href='/upload'>Upload File</a>"
 
 @app.route('/login')
 def login():
+    global is_member 
+    is_member = False
     scope = 'user-read-private user-read-email playlist-modify-private'  # Add any additional scopes you need
     auth_url = f"{SPOTIFY_AUTH_URL}?client_id={SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri={SPOTIFY_REDIRECT_URI}&scope={scope}"
     return redirect(auth_url)
-    
-#    scope = 'user-read-private'
-#    params = {
-#        'client_id': client_id,
-#        'response_type': 'code',
-#        'scope': scope,
-#        'redirect_uri': redirect_uri,
-#        'show_dialog' : False
-#    }
-#    auth_url_return = f"{auth_url}?{urllib.parse.urlencode(params)}"
-#    
-#    return redirect(auth_url_return)
+
+@app.route('/memberin')
+def memerin():
+    global is_member
+    is_member = True
+    scope = 'user-read-private user-read-email playlist-modify-private'  # Add any additional scopes you need
+    auth_url = f"{SPOTIFY_AUTH_URL}?client_id={SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri={SPOTIFY_REDIRECT_URI}&scope={scope}"
+    return redirect(auth_url)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' in request.files:
+        file = request.files['file']
+        if file.filename != '':
+            filename = secure_filename(file.filename)
+            extension = os.path.splitext(filename)[1]
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], 'image' + extension)
+            file.save(save_path)
+            # Note that 'static' should not be included in the filename argument here
+            image_url = url_for('static', filename=f'images/image{extension}')
+            return jsonify({'imagePath': image_url})
+    return redirect(url_for('index'))
+
 
 @app.route('/callback')
 def callback():
@@ -66,8 +92,11 @@ def callback():
     session['access_token'] = token_info.get('access_token')
     session['refresh_token'] = token_info.get('refresh_token')
     session['expires_at'] = datetime.now().timestamp() + token_info.get('expires_in', 0)
-
-    return redirect('/playlists')
+    
+    if is_member:
+        return redirect(url_for('make_moment_playlist'))
+    else:
+        return redirect(url_for('make_playlists'))
 
 @app.route('/playlists')
 def make_playlists():
@@ -93,10 +122,14 @@ def make_playlists():
     }
     user_info = requests.get(f"{api_base_url}me", headers=headers).json()
     user_id = user_info['id']
-
+    
+    today = date.today()
+    d1 = today.strftime("%d/%m/%Y")
+    name = 'Your Day Wrapped: ' + d1
+    
     playlist_data = {
-        'name': 'New Playlist',
-        'description': 'Playlist created from .txt file',
+        'name': name,
+        'description': 'This is what you heard today',
         'public': False  # Set to True if you want the playlist to be public
     }
     create_response = requests.post(f"{api_base_url}users/{user_id}/playlists", headers=headers, json=playlist_data)
@@ -109,7 +142,6 @@ def make_playlists():
     # Redirect or send a success response
     return redirect('/success')  # Replace with your success page
 
-    
     
 @app.route('/refresh-token')
 def refresh_token():
@@ -125,11 +157,58 @@ def refresh_token():
     
     session['access_token'] = new_token_info['access_token']
     session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
-    return redirect('/playlists')
+    return redirect('/login')
 
 @app.route('/success')
 def end():
-    return "How did you make it here"
+    return "Welcome <a href='/login'>Login</a> & <a href='/memberin'>Login-member</a> & <a href='/upload'>Upload File</a>"
+
+@app.route('/playlists-members')
+def make_moment_playlist():
+    # Check if the access token is available and valid
+    if 'access_token' not in session or 'expires_at' not in session or session['expires_at'] < datetime.now().timestamp():
+        return redirect('/login')  # or handle token refresh
+    
+    if (os.listdir('static/images')[0] == "image.jpg"):
+        song_urls = findSong("static/images/image.jpg")
+    elif (os.listdir('static/images')[0] == "image.jpeg"):
+        song_urls = findSong("static/images/image.jpeg")
+    else:
+        song_urls = findSong("static/images/image.png")
+
+    # Extract track IDs from URLs
+    track_ids = [url.split('/')[-1].strip() for url in song_urls]
+
+    # Create a new playlist
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}",
+        'Content-Type': 'application/json'
+    }
+    user_info = requests.get(f"{api_base_url}me", headers=headers).json()
+    user_id = user_info['id']
+
+    playlist_data = {
+        'name': 'Your Moment.....',
+        'description': 'Heres some music for what you are doing right now',
+        'public': False  # Set to True if you want the playlist to be public
+    }
+    create_response = requests.post(f"{api_base_url}users/{user_id}/playlists", headers=headers, json=playlist_data)
+    playlist_id = create_response.json()['id']
+    
+    selected_songs = random.sample(song_urls, 2)
+    with open("songs.txt", "a") as file:
+        for song in selected_songs:
+            file.write(song + "\n")
+    
+    # Add tracks to the playlist
+    add_tracks_data = {'uris': [f'spotify:track:{track_id}' for track_id in track_ids]}
+    requests.post(f"{api_base_url}playlists/{playlist_id}/tracks", headers=headers, json=add_tracks_data)
+    
+    playlist_link = f"https://open.spotify.com/playlist/{playlist_id}"
+    
+    # Redirect or send a success response
+    return jsonify({'playlistId': playlist_link})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
